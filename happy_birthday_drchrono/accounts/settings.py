2b36a6c66bdb4905aa5a3c8cf023dcbc85f5e5f5
Timothy Best <besttimothy1@gmail.com>
2015-08-17 15:06:55 -0400

    added a user profile

--- happy_birthday_drchrono/accounts/settings.py
+++ happy_birthday_drchrono/accounts/settings.py
@@ -1,7 +1,19 @@
 import os
 
+LOGIN_URL = '/login/'
+LOGIN_REDIRECT_URL = '/profile/'
+# URL_PATH = ''
+# SOCIAL_AUTH_STRATEGY = 'social.strategies.django_strategy.DjangoStrategy'
+# SOCIAL_AUTH_STORAGE = 'social.apps.django_app.default.models.DjangoStorage'
+# SOCIAL_AUTH_EMAIL_FORM_URL = '/signup-email'
+# SOCIAL_AUTH_EMAIL_FORM_HTML = 'email_signup.html'
+# SOCIAL_AUTH_EMAIL_VALIDATION_FUNCTION = 'example.app.mail.send_validation'
+# SOCIAL_AUTH_EMAIL_VALIDATION_URL = '/email-sent/'
+# SOCIAL_AUTH_USERNAME_FORM_URL = '/signup-username'
+# SOCIAL_AUTH_USERNAME_FORM_HTML = 'username_signup.html'
+
 SOCIAL_AUTH_URL_NAMESPACE = 'social'
-# TDOD: fix pipeline so registered users can authenticate
+# TODO: fix pipeline so registered users can authenticate
 SOCIAL_AUTH_PIPELINE = (
     'social.pipeline.social_auth.social_details',
     'social.pipeline.social_auth.social_uid',
--- happy_birthday_drchrono/accounts/views.py
+++ happy_birthday_drchrono/accounts/views.py
@@ -1,8 +1,19 @@
-from django.shortcuts import redirect
 from django.contrib.auth import logout as auth_logout
+from django.contrib.auth.decorators import login_required
+from django.views.generic.base import TemplateView
+from django.shortcuts import redirect
 
 
 def logout(request):
     """Logs out user"""
     auth_logout(request)
     return redirect('/')
+
+class ProfileView(TemplateView):
+    template_name='accounts/profile.html'
+
+    def get_context_data(self, **kwargs):
+        context = super(ProfileView, self).get_context_data(**kwargs)
+        return context
+
+profile = login_required(ProfileView.as_view())
