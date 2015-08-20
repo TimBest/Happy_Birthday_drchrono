f8609107bccab069609bc5dcd0af37793b94f5c2
Timothy Best <besttimothy1@gmail.com>
2015-08-20 13:50:12 -0400

    message text is now parsed for variables

--- happy_birthday_drchrono/greetings/notifications.py
+++ happy_birthday_drchrono/greetings/notifications.py
@@ -22,12 +22,36 @@ def send_happy_birthdays():
 
         for patient in greeting.user.patients.filter(date_of_birth__month=today.month, date_of_birth__day=today.day):
             if patient.email:
-                send_email(greeting.email_subject, greeting.email_body, [patient.email,])
+                send_email(
+                    evalualte_variables(patient, greeting.email_subject),
+                    evalualte_variables(patient, greeting.email_body),
+                    [patient.email,]
+                )
 
     for greeting in sms_greetings:
         update_patients_for_user(greeting.user, greeting.last_ran)
         today = datetime.datetime.now().today()
 
         for patient in greeting.user.patients.filter(date_of_birth__month=today.month, date_of_birth__day=today.day):
-            if patient.email:
-                send_sms(greeting.sms, patient.cell_phone)
+            if patient.cell_phone:
+                send_sms(
+                    evalualte_variables(patient, greeting.sms),
+                    patient.cell_phone
+                )
+
+def ord(n):
+    return str(n)+("th" if 4<=n%100<=20 else {1:"st",2:"nd",3:"rd"}.get(n%10, "th"))
+
+def evalualte_variables(patient, message=''):
+    age = ord(datetime.datetime.now().today().year - patient.date_of_birth.year)
+    variables = {
+        "$patients-first-name": patient.first_name,
+        "$patients-last-name": patient.last_name,
+        "$doctors-name": patient.doctor,
+        "$patients-age": age,
+    }
+    message_parts = message.split('\$')
+    for i in range(len(message_parts)):
+        for variable, value in variables.iteritems():
+            message_parts[i] = message_parts[i].replace(variable, value)
+    return '$'.join(message_parts)
